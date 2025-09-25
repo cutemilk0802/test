0d66b8d114afe5c5e10b6913b97cebc863e9f079
cutemilk0802 <cutemilk0802@gmail.com>
2025-09-25 17:53:12 +0900

    素因数分解プログラムの更新

---
+++ 素因数分解.py
@@ -0,0 +1,17 @@
+print("素因数分解したい数字 ")
+num = int(input())
+count = 1
+sosuu = 2
+while num/sosuu >=1:
+    sosuu = 2
+    count +=1
+    for i in range(count-2):
+        if count%(i+2) == 0:
+            break
+        else:
+            sosuu +=1
+    if sosuu == count :
+        if num % sosuu ==0:
+            num /= sosuu
+            print(sosuu)  
+            count -=1
